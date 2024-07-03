caabf1174048d6d8c71ca5ece49539ad4131f181
sonvtkt104 <nguyenxuanson.kt162@gmail.com>
2024-07-03 18:32:53 +0700

    game v1

---
+++ src/pages/GamePokemon/data/index.js
@@ -0,0 +1,86 @@
+const infoPokemon = [
+    {
+        image: '',
+        value: 1,
+    },
+    {
+        image: '',
+        value: 2,
+    },
+    {
+        image: '',
+        value: 3,
+    },
+    {
+        image: '',
+        value: 4,
+    },
+    {
+        image: '',
+        value: 5,
+    },
+    {
+        image: '',
+        value: 6,
+    },
+    {
+        image: '',
+        value: 7,
+    },
+    {
+        image: '',
+        value: 8,
+    },
+    {
+        image: '',
+        value: 9,
+    },
+    {
+        image: '',
+        value: 10,
+    },
+    {
+        image: '',
+        value: 11,
+    },
+    {
+        image: '',
+        value: 12,
+    },
+    {
+        image: '',
+        value: 13,
+    },
+    {
+        image: '',
+        value: 14,
+    },
+    {
+        image: '',
+        value: 15,
+    },
+    {
+        image: '',
+        value: 16,
+    },
+    {
+        image: '',
+        value: 17,
+    },
+    {
+        image: '',
+        value: 18,
+    },
+    {
+        image: '',
+        value: 19,
+    },
+    {
+        image: '',
+        value: 20,
+    },
+];
+
+export {
+    infoPokemon
+}
--- src/pages/GamePokemon/index.jsx
+++ src/pages/GamePokemon/index.jsx
@@ -1,9 +1,81 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import './css/index.css';
+import { infoPokemon } from "./data";
+
+const widthMap = 16 // + 2 (Outline)
+const heightMap = 9 // + 2 (Outline)
 
 export default function GamePokemon() {
+    const [listPokemon, setListPokemon] = useState([[]]) // [x][y] = infoPokemon
+    
+
+    useEffect(() => {
+        let arr = []
+        for(let i = 0; i < widthMap + 2; i++) {
+            arr[i] = []
+        }
+    
+        let number = 0
+        let indexPokemon = 0
+        let arrPokemon = [];
+        while (arrPokemon.length < (widthMap * heightMap)) {
+            if(number == 2) {
+                number = 0
+                indexPokemon ++
+            }
+
+            if(!infoPokemon[indexPokemon]) {
+                indexPokemon = 0
+            }
+
+            if(infoPokemon[indexPokemon]) {
+                arrPokemon.push(infoPokemon[indexPokemon])
+                number = number + 1
+            }
+        }
+
+        // shuffle
+        arrPokemon = arrPokemon?.sort(() => Math.random() - 0.5)
+      
+        let i = 0
+        for(let x = 1; x <= widthMap; x++) {
+            for(let y = 1; y <= heightMap; y++) {
+                arr[x][y] = arrPokemon[i]
+                i++
+            }
+        }
+
+        console.log('listPokemon', arr)
+        setListPokemon(arr)
+    }, [])
+
     return (
-        <div>
-            game pokemon
+        <div
+            style={{
+                display: "grid",
+                gridTemplateColumns:  `repeat(${widthMap + 2}, 1fr)`,
+                gridTemplateRows: `repeat(${heightMap + 2}, 1fr)`,
+                gap: "1px",
+                height: "100%",
+                width: "100%",
+            }}
+        >
+            {
+                listPokemon?.map((col, x) => {
+                    return col.map((pokemon, y) => (
+                        <span
+                            style={{
+                                gridColumnStart: x + 1,
+                                gridRowStart: y + 1,
+                                textAlign: "center",
+                                border: '1px solid'
+                            }}      
+                        >
+                            {pokemon?.value}
+                        </span>
+                    ))
+                })
+            }
         </div>
     )
 }
